c5060e0e1e04381a4a0cce827cb31a62e357e38e
network-flows <nzh21@mails.tsinghua.edu.cn>
2024-05-08 01:27:38 +0800

    typo fix & added count on example talent

--- TutorialMod_code/Patches/TA_CoinGain.cs
+++ TutorialMod_code/Patches/TA_CoinGain.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tool.Database;
 
 namespace TutorialMod
 {
     public class TA_CoinGain: TalentSkill, IBattleEnd
     {
+        public override void Init(ShipData shipData, Talent talent, DataTalent dataTalent)
+        {
+            base.Init(shipData, talent, dataTalent);
+            SignProcessText();
+        }
+
         public void OnBattleEnd()
         {
             WantedManager.Instance().wantedProcess.AddCoin(args[0]);
+            this.count.Value += args[0];
             Blink();
+        }
+
+        protected override void ProcessText()
+        {
+            if (slotResult != null)
+            {
+                slotResult.processText.text = this.count.Value.ToString();
+            }
         }
     }
 }
--- docs/Content.md
+++ docs/Content.md
@@ -8,7 +8,7 @@ The following checked items can be easily added to your mod with csv (similar to
 
 - [x] [Units](ShipUnit_EN.md)
 - [x] [Treasures](#Treasures)
-- [x] [Talents](#talents)
+- [x] [Talents](#Talents)
 - [x] [Pilots](#Pilots)
 - [ ] Player Ships
 - [ ] Enemy Ships
